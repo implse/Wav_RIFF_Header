f9b3c010174a44132fc43cffde397b15e59b9471
Finite <finite.impulse@gmail.com>
2022-04-16 14:33:04 +0200

    Add type

--- wav_header.py
+++ wav_header.py
@@ -1,10 +1,15 @@
 import sys
 
+sample_id = sys.argv[1]
+
 # Open a .wav file as read binary
-with open(sys.argv[1], 'rb') as fp:
+with open(sample_id, 'rb') as fp:
     raw_wav = fp.readlines()
 
-print('WAV RIFF Header (44 bytes)')
+print(f'Sample: {sample_id.split("/")[-1]}')
+
+
+print('WAV RIFF Header Description (44 bytes)')
 print("-" * 20)
 
 # ChunkID: 4 bytes (big)
@@ -13,7 +18,7 @@ chunkID_hex = "/".join(hex(i) for i in chunkID)
 chunkID_decode = chunkID.decode('ascii')
 
 print(f'ChunkID 4 bytes (big): {chunkID_hex}')
-print(f'ChunkID decode: {chunkID_decode}')
+print(f'ChunkID (RIFF): {chunkID_decode}')
 print("-" * 20)
 
 # ChunkSize: 4 bytes (little) This is the size of the entire file in bytes minus 8 bytes (not included in this count, ChunkID and ChunkSize)
@@ -21,7 +26,7 @@ chunkSize = raw_wav[0][4:8]
 chunkSize_decode = int.from_bytes(chunkSize, 'little')
 
 print(f'ChunkSize 4 bytes (little): {"/".join(hex(i) for i in chunkSize)}')
-print(f'ChunkSize decode: {chunkSize_decode}')
+print(f'ChunkSize (bytes): {chunkSize_decode}')
 print("-" * 20)
 
 # Format: 4 bytes (big)
@@ -29,7 +34,7 @@ format = raw_wav[0][8:12]
 format_decode = format.decode('ascii')
 
 print(f'Format 4 bytes (big): {"/".join(hex(i) for i in format)}')
-print(f'Format decode: {format_decode}')
+print(f'Format (WAVE): {format_decode}')
 print("-" * 20)
 
 # Subchunck1ID: 4 bytes
@@ -38,7 +43,7 @@ Subchunck1ID_decode = Subchunck1ID.decode('ascii')
 
 
 print(f'Subchunck1ID 4 bytes: {"/".join(hex(i) for i in Subchunck1ID)}')
-print(f'Subchunck1ID decode: {Subchunck1ID_decode}')
+print(f'Subchunck1ID (fmt): {Subchunck1ID_decode}')
 print("-" * 20)
 
 # Subchunck1Size: 4 bytes (little)
@@ -46,7 +51,7 @@ Subchunck1Size = raw_wav[0][16:20]
 Subchunck1Size_decode = int.from_bytes(Subchunck1Size, 'little') 
 
 print(f'Subchunck1Size 4 bytes (little): {"/".join(hex(i) for i in Subchunck1Size)}')
-print(f'Subchunck1Size decode : {Subchunck1Size_decode}')
+print(f'Subchunck1Size (16 for PCM): {Subchunck1Size_decode}')
 print("-" * 20)
 
 # Audio format: 2 bytes (little)
@@ -55,7 +60,7 @@ audio_format_decode = int.from_bytes(audio_format, 'little')
 
 
 print(f'Audio format 2 bytes (little) : {"/".join(hex(i) for i in audio_format)}')
-print(f'Audio format decode: {audio_format_decode}')
+print(f'Audio format (PCM=1): {audio_format_decode}')
 print("-" * 20)
 
 # Number of channels: 2 bytes (little)
@@ -64,7 +69,7 @@ numChannels_decode = int.from_bytes(numChannels, 'little')
 
 
 print(f'Number of Channels 2 bytes (little): {"/".join(hex(i) for i in numChannels)}')
-print(f'Number of Channels decode: {numChannels_decode}')
+print(f'Number of Channels: {numChannels_decode}')
 print("-" * 20)
 
 # Sample Rate: 4 bytes (little)
@@ -73,7 +78,7 @@ SampleRate_decode = int.from_bytes(SampleRate, 'little')
 
 
 print(f'Sample Rate 4 bytes (little): {"/".join(hex(i) for i in SampleRate)}')
-print(f'Sample Rate decode: {SampleRate_decode}')
+print(f'Sample Rate (Hz): {SampleRate_decode}')
 print("-" * 20)
 
 # Byte Rate: 4 bytes (little)
@@ -81,7 +86,7 @@ ByteRate = raw_wav[0][28:32]
 ByteRate_decode = int.from_bytes(ByteRate, 'little')
 
 print(f'Byte Rate 4 bytes (little): {"/".join(hex(i) for i in ByteRate)}')
-print(f'Byte Rate decode: {ByteRate_decode}')
+print(f'Byte Rate (bytes): {ByteRate_decode}')
 print("-" * 20)
 
 # Block Align: 2 bytes (little The number of bytes for one sample )
@@ -89,7 +94,7 @@ BlockAlign = raw_wav[0][32:34]
 BlockAlign_decode = int.from_bytes(BlockAlign, 'little')
 
 print(f'BlockAlign 2 bytes (little): {"/".join(hex(i) for i in BlockAlign)}')
-print(f'BlockAlign: {BlockAlign_decode}')
+print(f'BlockAlign (bytes): {BlockAlign_decode}')
 print("-" * 20)
 
 # Bit Depth: 2 bytes (little)
@@ -97,7 +102,7 @@ BitsPerSample = raw_wav[0][34:36]
 BitsPerSample_decode = int.from_bytes(BitsPerSample, 'little')
 
 print(f'Bits Per Sample 2 bytes (little): {"/".join(hex(i) for i in BitsPerSample)}')
-print(f'Bits Per Sample: {BitsPerSample_decode}')
+print(f'Bits Per Sample (bits): {BitsPerSample_decode}')
 print("-" * 20)
 
 # SubChunck2ID: 4 bytes (big)
@@ -114,4 +119,4 @@ Subchunk2Size = raw_wav[0][40:44]
 Subchunk2Size_decode = int.from_bytes(Subchunk2Size, 'little')
 
 print(f'Subchunk2Size 4 bytes (little): {"/".join(hex(i) for i in Subchunk2Size)}')
-print(f'Subchunk2Size: {Subchunk2Size_decode}')
+print(f'Subchunk2Size (bytes): {Subchunk2Size_decode}')
